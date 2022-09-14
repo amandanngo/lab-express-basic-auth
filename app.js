e03421042a2101773541de94bc5c7a3c2bf84929
Amanda Ngo <amanda.ngo172@gmail.com>
2022-09-14 08:54:55 -0500

    Cookies are not working

--- app.js
+++ app.js
@@ -12,11 +12,36 @@ const express = require('express');
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
 const hbs = require('hbs');
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo');
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
+
+
+    app.set('trust proxy', 1);
+    app.use(
+        session({
+            // secret: process.env.SESS_SECRET,
+            secret: 'key',
+            resave: true,
+            saveUninitialized: false,
+            cookie: {
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+                secure: process.env.NODE_ENV === 'production',
+                httpOnly: true,
+                maxAge: 60000 // 60 * 1000 ms === 1 min
+            },
+            store: MongoStore.create({
+                mongoUrl: 'mongodb://localhost/lab-express-basic-auth'     })
+        })
+    )
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // default value for title local
 const projectName = 'lab-express-basic-auth';
@@ -28,8 +53,13 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 const index = require('./routes/index');
 app.use('/', index);
 
+const authRoutes = require('./routes/auth.routes');
+app.use('/', authRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
 module.exports = app;
 
+
+app.listen(3000, () => console.log('Server is running'))
---
+++ routes/auth.routes.js
@@ -0,0 +1,66 @@
+const Express = require('express');
+const router = Express.Router();
+const User = require('../models/User.model');
+const bcryptjs = require('bcryptjs');
+const { isAuthenticated, isNotAuthenticated } = require('../middlewares/auth.middleware');
+
+
+router.get('/signup', isNotAuthenticated, (req,res,next) => {
+    res.render('signup')
+})
+
+router.post('/signup', (req,res,next) => {
+    const myUsername = req.body.username;
+    const myPassword = req.body.password;
+
+    const myHashedPass = bcryptjs.hashSync(myPassword);
+
+    User.create({
+        username: myUsername,
+        password: myHashedPass
+    })
+        .then(savedUser => {
+            res.send(savedUser)
+        })
+        .catch(err => {
+            res.send(err)
+        })
+})
+
+router.get('/login', isNotAuthenticated, (req,res,next) => {
+    res.render('login')
+})
+
+router.post('/login', (req,res,next) => {
+    console.log(req.body)
+
+    const myUsername = req.body.username;
+    const myPassword = req.body.password;
+
+    User.findOne({
+        username: myUsername
+    })
+        .then(foundUser => {
+            console.log(foundUser);
+
+            if(!foundUser){
+                res.send('no user matching this username')
+            }
+
+            const isValidPassword = bcryptjs.compareSync(myPassword,foundUser.password)
+
+            if(!isValidPassword){
+                res.send('incorrect password')
+            }
+            res.render('profile', {
+                username: foundUser.username
+            });
+        })
+        .catch(err => res.send(err))
+})
+
+router.get('/profile', isAuthenticated, (req,res,next) => {
+    res.render('profile')
+})
+
+module.exports = router;
